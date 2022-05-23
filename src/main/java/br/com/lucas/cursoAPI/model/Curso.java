940a5898f225b2be690e79461414398eb8b616af
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-05-23 08:40:05 -0300

    Specified Id attributes

--- src/main/java/br/com/lucas/cursoAPI/model/Curso.java
+++ src/main/java/br/com/lucas/cursoAPI/model/Curso.java
@@ -1,13 +1,15 @@
 package br.com.lucas.cursoAPI.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="curso")
 public class Curso {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nomecurso;
 
     public Long getId() {
