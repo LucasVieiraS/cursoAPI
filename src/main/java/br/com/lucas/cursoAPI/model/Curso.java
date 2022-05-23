2b5855cffb8f663f4a415d809a103898e56ffe5f
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-05-23 08:30:13 -0300

    Imported @Table/@Entity

--- src/main/java/br/com/lucas/cursoAPI/model/Curso.java
+++ src/main/java/br/com/lucas/cursoAPI/model/Curso.java
@@ -1,5 +1,10 @@
 package br.com.lucas.cursoAPI.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="curso")
 public class Curso {
 
     private Long id;
